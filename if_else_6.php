313a1656e8620f90af8ade311608deacd2f19f52
2aditya2005 <adityaavahd2005@gmail.com>
2025-03-26 00:38:52 +0530

    if else and swich case and while loops are add

---
+++ if_else_6.php
@@ -0,0 +1,40 @@
+<?php
+
+// if else in php
+$a = 665;
+$b = 9;
+
+if ($a > 78){
+    echo "a is greter than 78";
+}
+else{
+    echo " a is not greter than 78";
+}
+
+echo "<br>";
+
+
+
+//nested if ifelse else
+
+echo "--------nested if else-------------<br>";
+$age = 54;
+
+
+
+if($age>18){
+
+echo "you can drink water with chai and alcohol";
+}
+
+elseif( $age > 13){
+    echo "you can drink chai only only with water. NO alcohol for you";
+
+}
+else{
+    echo "you can drink water only";
+
+    
+}
+echo "DONE";
+?>
---
+++ switch_7.php
@@ -0,0 +1,22 @@
+<?php
+
+$age = 12;
+
+switch($age){
+    case 12:
+        echo "you age 12 years old<br>";
+        break;
+
+        case 45:
+            echo "you age 45 years old <br>";
+            break;
+        
+        case 56:
+            echo "you age 56 years old ";
+            break;
+        
+        default:
+        echo "your age is weird ";
+        break;
+        
+    }
