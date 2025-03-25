b778f90496edf7f092ea3eeede339c09c68e8624
2aditya2005 <adityaavahd2005@gmail.com>
2025-03-25 18:13:22 +0530

    chage file name

---
+++ variable1.php
@@ -0,0 +1,18 @@
+
+<?php
+// php not case sensetive
+echo "this is Tutorial 1<br>";
+ECHO "this is Tutorial 2<br>";
+//-----------------Variable basic------------------//
+
+//variables are containers for storing information
+//$starts with a $
+// variable is case sensetive 
+$name = "ADITYA";
+$Name = "ADI";
+$income = 200;
+echo "this guy is $name and his income is Rs. $income";
+
+echo "$name is a good boy<br>";
+echo "$name is the real gangstar<br>";
+?>
